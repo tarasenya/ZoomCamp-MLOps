85746065e10a8fd56361413928f8ee98180bcfe6
Taras <tarasenya@gmail.com>
2023-05-19 14:21:43 +0200

    Changes train.py to perform mlflow autologging of sklearn model

--- Week-2/homework/train.py
+++ Week-2/homework/train.py
@@ -1,10 +1,13 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+# to start mlflow server with mlflow.db backend store and artifacts folder to store artifacts
+# mlflow server --backend-store-uri sqlite:///mlflow.db --artifacts-destination ./artifacts
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -18,16 +21,17 @@ def load_pickle(filename: str):
     help="Location where the processed NYC taxi trip data was saved"
 )
 def run_train(data_path: str):
-
+    mlflow.set_tracking_uri('http://127.0.0.1:5000')
+    mlflow.set_experiment('green_taxi')
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.start_run():
+        mlflow.sklearn.autolog()
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
-
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
-
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 if __name__ == '__main__':
     run_train()
