68e278b812f84871ad20ca47ac2f5aaab3205336
Taras <tarasenya@gmail.com>
2023-06-03 13:51:32 +0200

    Added solutions to homework from seminar W&B

--- Week-2/workshop_wnb/train.py
+++ Week-2/workshop_wnb/train.py
@@ -23,11 +23,11 @@ def load_pickle(filename: str):
 @click.option("--random_state", default=0, help="Random state")
 @click.option("--max_depth", default=10, help="Max tree depth")
 def run_train(
-    wandb_project: str,
-    wandb_entity: str,
-    data_artifact: str,
-    max_depth: int,
-    random_state: int,
+        wandb_project: str,
+        wandb_entity: str,
+        data_artifact: str,
+        max_depth: int,
+        random_state: int,
 ):
     # Initialize a Weights & Biases run
     wandb.init(
@@ -44,19 +44,21 @@ def run_train(
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    # Define the XGBoost Regressor Mode, train the model and perform prediction
+    # Define the RandomForestRegressor, train the model and perform prediction
     rf = RandomForestRegressor(max_depth=max_depth, random_state=random_state)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_val)
 
     mse = mean_squared_error(y_val, y_pred, squared=False)
-    # TODO: Log `mse` to Weights & Biases under the key `"MSE"`
-
+    wandb.log({'MSE': mse})
     with open("regressor.pkl", "wb") as f:
         pickle.dump(rf, f)
 
     # TODO: Log `regressor.pkl` as an artifact of type `model`
-
+    _artifact = wandb.Artifact('green-taxi-model', type='model')
+    _artifact.add_file('regressor.pkl')
+    wandb.log_artifact(_artifact)
+    wandb.finish()
 
 if __name__ == "__main__":
     run_train()
