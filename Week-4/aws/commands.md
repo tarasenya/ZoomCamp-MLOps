89339fdf8ab78b281c88f8ac0c4ea304ba2c5766
Taras <tarasenya@gmail.com>
2023-06-27 20:25:18 +0200

    Added initial data for Week-5

---
+++ Week-4/aws/commands.md
@@ -0,0 +1,28 @@
+docker build -t stream-model-duration:v1 .
+
+docker run -it --rm \
+    -p 8080:8080 \
+    -e PREDICTIONS_STREAM_NAME="ride_predictions" \
+    -e TEST_RUN="True" \
+    -e AWS_DEFAULT_REGION="us-east-1" \
+    stream-model-duration:v1
+
+docker run -it --rm \
+    -p 8080:8080 \
+    -e PREDICTIONS_STREAM_NAME="ride_predictions" \
+    -e TEST_RUN="True" \
+    -e AWS_DEFAULT_REGION="us-east-1" \
+    -e AWS_ACCESS_KEY_ID="AKIAU6YSEVNWH74ZG55V" \
+    -e AWS_SECRET_ACCESS_KEY="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" \
+    -v /home/taras/.aws:/root/.aws \
+    stream-model-duration:v1
+
+REMOTE_URI="340951083884.dkr.ecr.us-east-1.amazonaws.com/duration-model"
+REMOTE_TAG="v1"
+REMOTE_IMAGE=${REMOTE_URI}:${REMOTE_TAG}
+
+LOCAL_IMAGE="stream-model-duration:v1"
+docker tag ${LOCAL_IMAGE} ${REMOTE_IMAGE}
+docker push ${REMOTE_IMAGE}
+
+aws ecr get-login-password | docker login --username AWS --password-stdin 340951083884.dkr.ecr.us-east-1.amazonaws.com/duration-model
---
+++ Week-4/aws/test.py
@@ -0,0 +1,25 @@
+import lambda_function
+
+event = {
+    "Records": [
+        {
+            "kinesis": {
+                "kinesisSchemaVersion": "1.0",
+                "partitionKey": "1",
+                "sequenceNumber": "49641808440514706213932692080310623236477433591655038978",
+                "data": "ewogICAgICAgICJyaWRlIjogewogICAgICAgICAgICAiUFVMb2NhdGlvbklEIjogMTMwLAogICAgICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICAgICAidHJpcF9kaXN0YW5jZSI6IDMuNjYKICAgICAgICB9LCAKICAgICAgICAicmlkZV9pZCI6IDE1NgogICAgfQ==",
+                "approximateArrivalTimestamp": 1687027572.021
+            },
+            "eventSource": "aws:kinesis",
+            "eventVersion": "1.0",
+            "eventID": "shardId-000000000000:49641808440514706213932692080310623236477433591655038978",
+            "eventName": "aws:kinesis:record",
+            "invokeIdentityArn": "arn:aws:iam::340951083884:role/lambda-kinesis-role",
+            "awsRegion": "us-east-1",
+            "eventSourceARN": "arn:aws:kinesis:us-east-1:340951083884:stream/ride_events"
+        }
+    ]
+}
+
+results = lambda_function.lambda_handler(event, None)
+print(results)
